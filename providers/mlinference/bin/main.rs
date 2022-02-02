dad97e13285d21696a56d9f7c945737123bdf825
Finnfalter <finnfalter@gmail.com>
2022-02-02 21:26:43 +0100

    refactored bindle functionality

--- providers/mlinference/bin/main.rs
+++ providers/mlinference/bin/main.rs
@@ -7,12 +7,12 @@ pub (crate) use wasmcloud_interface_mlinference::{
     MlError
 };
 use wasmcloud_provider_mlinference::{
-    load_settings, get_default_inference_result, ModelZoo, ModelContext, ModelMetadata,
-    get_first_member_of, TractEngine, InferenceEngine, Graph, GraphExecutionContext
+    load_settings, get_default_inference_result, ModelZoo, ModelContext,
+    TractEngine, InferenceEngine, Graph, GraphExecutionContext,
+    BindleLoader
 };
 use tokio::sync::RwLock;
-use bindle::{client};
-//use log::{debug, info, error};  
+use bindle::{client::{Client, tokens::NoToken}};
 
 use wasmbus_rpc::provider::prelude::*;
 
@@ -48,90 +48,45 @@ impl ProviderDispatch for MlinferenceProvider {}
 #[async_trait]
 impl ProviderHandler for MlinferenceProvider {
 
-    async fn put_link(&self, ld: &LinkDefinition) -> Result<bool, RpcError> {
-        let settings = load_settings(&ld.values).map_err(|e| RpcError::ProviderInit(e.to_string()))?;
+    async fn put_link(&self, ld: &LinkDefinition) -> Result<bool, RpcError> 
+    {
+        let settings = load_settings(&ld.values)
+            .map_err(|e| RpcError::ProviderInit(e.to_string()))?;
 
         let mut model_zoo: ModelZoo = ModelZoo::new();
         
         settings.models.zoo.iter().for_each(|(k,v)| {
             model_zoo
-            .insert(k.to_string(), ModelContext{ 
+            .insert(k.to_string(), ModelContext { 
                 bindle_url: v.to_string(), 
                 ..Default::default() 
             });
         });
         
-        // init the connection to bindle
-        let url = std::env::var("BINDLE_URL")
-            .map_err(|_| RpcError::InvalidParameter("No 'BINDLE_URL' defined, verify your bindle url.".into()))?;
-        
-        let bindle_client = client::Client::new(&url, client::tokens::NoToken)
-            .map_err(|_| RpcError::InvalidParameter("'BINDLE_URL' not valid, verify your bindle url.".into()))?;
+        let bindle_client: Client<NoToken> = BindleLoader::provide("BINDLE_URL")
+            .await
+            .map_err(|error| RpcError::ProviderInit(format!("{}", error)))?;
 
         for (_, context) in model_zoo.iter_mut() 
         {
-            let invoice = bindle_client.get_invoice(&context.bindle_url).await
-                .map_err(|_| RpcError::InvalidParameter(
-                    format!("invoice '{}' could not be fetched", &context.bindle_url)
-                ))?;
-
-            let parcels = invoice.parcel.ok_or(
-                RpcError::InvalidParameter(
-                    format!("invoice '{}' could not be fetched", &context.bindle_url)
-                ))?;
-
-            let model_parcel = get_first_member_of(&parcels, "model")
-                .map_err(|_| RpcError::InvalidParameter(
-                    format!("The invoice must have >0 parcels being member of group 'model'")
-                ))?;
-
-            let metadata_parcel = get_first_member_of(&parcels, "metadata")
-                .map_err(|_| RpcError::InvalidParameter(
-                    format!("The invoice must have >0 parcels being member of group 'metadata'")
-                ))?;
-
-            let model_data_blob: Vec<u8> = bindle_client
-                .get_parcel(&context.bindle_url, &model_parcel.label.sha256)
+            let downloads = BindleLoader::get_model_and_metadata(&bindle_client, &context.bindle_url)
                 .await
-                .map_err(|_| RpcError::ProviderInit(
-                    format!("could not download model {} from path {}", 
-                    model_parcel.label.name, &context.bindle_url)
-                ))?;
-            log::info!("successfully downloaded model {} of size {}", 
-                model_parcel.label.name, model_data_blob.len());
+                .map_err(|error| RpcError::ProviderInit(format!("{}", error)))?;
 
-            let metadata_blob: Vec<u8> = bindle_client
-                .get_parcel(&context.bindle_url, &metadata_parcel.label.sha256)
-                .await
-                .map_err(|_| RpcError::ProviderInit(
-                    format!("could not download metadata {} from path {}", 
-                        metadata_parcel.label.name, &context.bindle_url)
-                ))?;
-            log::info!("successfully downloaded metadata {} of size {}", 
-                metadata_parcel.label.name, metadata_blob.len());
-
-            // storing metadata makes sense when model data is done
-            let metadata: ModelMetadata = ModelMetadata::from_json(&metadata_blob)
-                .map_err(|error| RpcError::ProviderInit(
-                    format!("{}", error)
-                ))?;
+            let (metadata, model_data_bytes) = downloads;
 
             context.clone().load_metadata(metadata)
                 .map_err(|e| RpcError::InvalidParameter(format!("{:?}",e)))?;
 
-            let graph: Graph = self.engine.load(&model_data_blob, &context.graph_encoding, &context.execution_target)
+            let graph: Graph = self.engine.load(&model_data_bytes, &context.graph_encoding, &context.execution_target)
                 .await
-                .map_err(|error| RpcError::ProviderInit(
-                    format!("{}", error)
-                ))?;
+                .map_err(|error| RpcError::ProviderInit(format!("{}", error)))?;
 
             context.graph = graph;
 
             let gec: GraphExecutionContext = self.engine.init_execution_context(context.graph)
                 .await
-                .map_err(|error| RpcError::ProviderInit(
-                    format!("{}", error)
-                ))?;
+                .map_err(|error| RpcError::ProviderInit(format!("{}", error)))?;
 
             context.graph_execution_context = gec;
         }
@@ -145,9 +100,9 @@ impl ProviderHandler for MlinferenceProvider {
     /// Handle notification that a link is dropped
     async fn delete_link(&self, actor_id: &str) 
     {
-        let mut aw = self.actors.write().await;
+        let mut actor_lock = self.actors.write().await;
 
-        let model_zoo: &ModelZoo = match aw.get(actor_id) {
+        let model_zoo: &ModelZoo = match actor_lock.get(actor_id) {
             Some(mz) => mz,
             None     => { return(); }
         };
@@ -157,7 +112,7 @@ impl ProviderHandler for MlinferenceProvider {
             self.engine.drop_model_state(&context.graph, &context.graph_execution_context).await;
         }
 
-        aw.remove(actor_id);
+        actor_lock.remove(actor_id);
     }
 }
 
